09191c62ebb81ad87635c9b03767bc7b180d7cba
Ruslan <ruslanvlasenko1488@gmail.com>
2021-04-20 17:31:30 +0500

    Add plug a some functionality on TrelloBoardComparer and plug on TrelloCardComparer classes.

---
+++ classes/TrelloCardComparer.py
@@ -0,0 +1,9 @@
+from trello import *
+from .CompareResult import *
+
+
+class TrelloCardComparer:
+    def compare_cards(self,
+                      card_to_compare: Card,
+                      expected_card: Card) -> CompareResult:
+        pass
