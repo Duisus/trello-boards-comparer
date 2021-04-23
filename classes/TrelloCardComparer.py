7eaa4bee4526ea663dbf0891ab32a4a60caceecc
Denis Bautin <denisbduis@gmail.com>
2021-04-23 22:10:48 +0500

    Finished realize TrelloCardComparer, but didn't test it

--- classes/TrelloCardComparer.py
+++ classes/TrelloCardComparer.py
@@ -1,25 +1,58 @@
+import typing
+import re
+
 from trello import *
+
 from .CompareResult import *
-from typing import *
-from collections import Counter
 
 
 class TrelloCardComparer:
+    _text_link_re = re.compile(r"\[.+]\(\S+\)")
+    _mention_member_re = re.compile(r"@[a-zA-Z0-9_]+")
+
     @classmethod
     def compare_cards(cls,
                       card_to_compare: Card,
                       expected_card: Card) -> CompareResult:
         compare_result = CompareResult(TrelloElement.CARD, card_to_compare.name)
 
-        for item in cls._compare_all_checklists(card_to_compare, expected_card):
-            compare_result.add_inner_compare_result(item)
+        for result in cls._compare_all_checklists(card_to_compare, expected_card):
+            compare_result.add_inner_compare_result(result)
+
+        compare_result.add_inner_compare_result(
+            cls._compare_description(
+                card_to_compare.description, expected_card.description))
+
+        compare_result.add_inner_compare_result(
+            cls._compare_comments(
+                cls._get_comments(card_to_compare),
+                cls._get_comments(expected_card.description)))
+
+        for result in cls._compare_label(card_to_compare.labels, expected_card.labels):
+            compare_result.add_inner_compare_result(result)
+
+        compare_result.add_inner_compare_result(
+            cls._compare_cover(card_to_compare, expected_card))
+
+        compare_result.add_inner_compare_result(
+            cls._compare_due(card_to_compare, expected_card))
+
+        compare_result.add_inner_compare_result(
+            cls._compare_members(card_to_compare, expected_card))
+
+        compare_result.add_inner_compare_result(
+            cls._compare_attachments(card_to_compare, expected_card))
 
         return compare_result
 
     @classmethod
+    def _get_comments(cls, card: Card) -> typing.List[str]:
+        return [comment_data['data']['text'] for comment_data in card.comments]
+
+    @classmethod
     def _compare_all_checklists(cls,
                                 card_to_compare: Card,
-                                expected_card: Card) -> List[CompareResult]:
+                                expected_card: Card) -> typing.List[CompareResult]:
         compare_results = []
 
         checklists_to_compare = card_to_compare.checklists
@@ -89,8 +122,8 @@ class TrelloCardComparer:
 
     @classmethod
     def _compare_checklist_item(cls,
-                                checklist_item_to_compare: Dict,
-                                expected_checklist_item: Dict) -> CompareResult:
+                                checklist_item_to_compare: typing.Dict,
+                                expected_checklist_item: typing.Dict) -> CompareResult:
         compare_result = CompareResult(TrelloElement.CHECKLIST_ITEM,
                                        checklist_item_to_compare['name'])
 
@@ -125,13 +158,52 @@ class TrelloCardComparer:
         return compare_result
 
     @classmethod
-    def _compare_description(cls) -> CompareResult:
-        pass
+    def _compare_description(cls,
+                             description_to_compare: str,
+                             expected_description: str) -> CompareResult:
+        expected_description_without_link = cls._text_link_re.sub(' ', expected_description)
+        actual_description_without_link = cls._text_link_re.sub(' ', description_to_compare)
+
+        if expected_description_without_link != actual_description_without_link:
+            result = CompareResult(TrelloElement.DESCRIPTION,
+                                   compare_result_type=CompareResultType.INVALID_VALUE)
+            result.expected_value = expected_description
+            result.actual_value = description_to_compare
+            return result
+
+        return CompareResult(TrelloElement.DESCRIPTION)
+
+    @classmethod
+    def _compare_comments(cls,  # TODO fix (strange logic)
+                          comments_to_compare: typing.List[str],
+                          expected_comments: typing.List[str]):
+        if len(expected_comments) == 0 and len(comments_to_compare) != 0:
+            return CompareResult(
+                TrelloElement.COMMENT, compare_result_type=CompareResultType.HAS_EXTRA_ELEMENT)
+        elif len(expected_comments) != 0 and len(comments_to_compare) == 0:
+            return CompareResult(
+                TrelloElement.COMMENT, compare_result_type=CompareResultType.DOES_NOT_CONTAIN_ELEMENT)
+        elif len(expected_comments) == 0 and len(comments_to_compare) == 0:
+            return CompareResult(TrelloElement.COMMENT)
+
+        expected_first_comment = expected_comments[0]
+        actual_first_comment = comments_to_compare[0]
+
+        if cls._comment_has_mention(expected_first_comment):
+            if cls._comment_has_mention(actual_first_comment):
+                return CompareResult(TrelloElement.COMMENT)
+
+            return CompareResult(
+                TrelloElement.COMMENT,
+                "нет упоминания",
+                CompareResultType.DOES_NOT_CONTAIN_ELEMENT)
+
+        return CompareResult(TrelloElement.COMMENT)
 
     @classmethod
     def _compare_label(cls,
-                       labels_to_compare: List[Label],
-                       expected_labels: List[Label]) -> List[CompareResult]:
+                       labels_to_compare: typing.List[Label],
+                       expected_labels: typing.List[Label]) -> typing.List[CompareResult]:
         compare_results = []
 
         labels_to_compare_colors = (item.color for item in labels_to_compare)
@@ -161,17 +233,67 @@ class TrelloCardComparer:
         return compare_results
 
     @classmethod
-    def _check_cover(cls) -> CompareResult:
-        pass
+    def _compare_cover(cls,
+                       card_to_compare: Card,
+                       expected_card: Card):
+        return cls._compare_availability(
+            card_to_compare, expected_card, TrelloElement.COVER, cls._has_cover)
 
     @classmethod
-    def _check_due(cls) -> CompareResult:
-        pass
+    def _compare_due(cls,
+                     card_to_compare: Card,
+                     expected_card: Card):
+        return cls._compare_availability(
+            card_to_compare, expected_card, TrelloElement.DUE, cls._has_due)
 
     @classmethod
-    def _check_members(cls) -> CompareResult:
-        pass
+    def _compare_members(cls,
+                         card_to_compare: Card,
+                         expected_card: Card):
+        return cls._compare_availability(
+            card_to_compare, expected_card, TrelloElement.MEMBER, cls._has_member)
 
     @classmethod
-    def _check_attachments(cls) -> CompareResult:
-        pass
+    def _compare_attachments(cls,
+                             card_to_compare: Card,
+                             expected_card: Card):
+        return cls._compare_availability(
+            card_to_compare, expected_card, TrelloElement.ATTACHMENT, cls._has_attachment)
+
+    @classmethod
+    def _compare_availability(cls,
+                              card_to_compare: Card,
+                              expected_card: Card,
+                              element: TrelloElement,
+                              check_availability_func) -> CompareResult:
+        if check_availability_func(card_to_compare) == check_availability_func(expected_card):
+            return CompareResult(element)
+
+        elif check_availability_func(card_to_compare):
+            return CompareResult(
+                element,
+                compare_result_type=CompareResultType.DOES_NOT_CONTAIN_ELEMENT)
+
+        return CompareResult(
+            element,
+            compare_result_type=CompareResultType.HAS_EXTRA_ELEMENT)
+
+    @classmethod
+    def _has_cover(cls, card: Card) -> bool:
+        return card._json_obj['cover']['idUploadedBackground'] is not None
+
+    @classmethod
+    def _has_due(cls, card: Card) -> bool:
+        return card.due is not None
+
+    @classmethod
+    def _has_member(cls, card: Card) -> bool:
+        return len(card.member_id) != 0
+
+    @classmethod
+    def _has_attachment(cls, card: Card) -> bool:
+        return len(card.attachments) != 0
+
+    @classmethod
+    def _comment_has_mention(cls, comment: str):
+        return cls._mention_member_re.search(comment) is not None
