a71360606322201840a8bee7bc121e5eaa6bdfe9
Denis Bautin <denisbduis@gmail.com>
2021-05-04 20:55:46 +0500

    Add new tests for CompareResult and refactor old

--- tests/CompareResult_tests.py
+++ tests/CompareResult_tests.py
@@ -38,9 +38,38 @@ class CompareResultTests(unittest.TestCase):
         self = CompareResult(TrelloElement.BOARD, '', CompareResultType.FAILED)
         assert self in list(self.get_not_success_results())
 
-    def test_get_no_success_result_contains_all_failed_results(self):
-        self = CompareResult(TrelloElement.BOARD, 'еуые', CompareResultType.FAILED)
-        self.add_inner_compare_result(self)
-        inner = CompareResult(TrelloElement.LIST,  "", CompareResultType.FAILED)
-        self.add_inner_compare_result(inner)
-        assert self in self._inner_compare_results and inner in self._inner_compare_results
+    def test_get_not_success_result_contains_all_not_success_results(self):
+        upper = CompareResult(TrelloElement.BOARD, 'еуые', CompareResultType.FAILED)
+        inner = CompareResult(TrelloElement.LIST, "", CompareResultType.DOES_NOT_CONTAIN_ELEMENT)
+        upper.add_inner_compare_result(inner)
+        self.assertCountEqual(
+            (upper, inner),
+            upper.get_not_success_results())
+
+    def test_get_not_success_result_does_not_return_success_CompareResults(self):
+        upper = CompareResult(TrelloElement.BOARD, compare_result_type=CompareResultType.FAILED)
+        upper.add_inner_compare_result(
+            CompareResult(TrelloElement.LIST, compare_result_type=CompareResultType.FAILED))
+        success_result = CompareResult(
+            TrelloElement.LIST, compare_result_type=CompareResultType.SUCCESS)
+        upper.add_inner_compare_result(success_result)
+
+        assert success_result not in upper.get_not_success_results()
+
+    def test_get_not_success_result_order_is_like_in_depth_first_search(self):  # todo refactor
+        names = ["1", "1.1", "1.1.1", "1.1.2", "1.2"]
+        upper_result = CompareResult(
+            TrelloElement.LIST, names[0], CompareResultType.FAILED)
+        inner_result = CompareResult(
+            TrelloElement.CARD, names[1], CompareResultType.FAILED)
+        inner_result.add_inner_compare_result(
+            CompareResult(TrelloElement.DESCRIPTION, names[2], CompareResultType.INVALID_VALUE))
+        inner_result.add_inner_compare_result(
+            CompareResult(TrelloElement.COVER, names[3], CompareResultType.DOES_NOT_CONTAIN_ELEMENT))
+        upper_result.add_inner_compare_result(inner_result)
+        upper_result.add_inner_compare_result(
+            CompareResult(TrelloElement.CARD, names[4], CompareResultType.HAS_EXTRA_ELEMENT))
+
+        actual = [result.compared_element_name for result in upper_result.get_not_success_results()]
+
+        self.assertEqual(names, actual)
