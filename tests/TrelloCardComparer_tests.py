5af9d3c44c5cf7f6141468c4ce9f2868eca8eba0
azyablic <anna.zyablic@gmail.com>
2021-05-04 23:54:30 +0500

    Add some tests for TrelloCardComparer

--- tests/TrelloCardComparer_tests.py
+++ tests/TrelloCardComparer_tests.py
@@ -37,3 +37,36 @@ class TrelloCardComparerTests(unittest.TestCase):
     def test_mention_in_comments(self, compared_card_id, expected_result=None):
         expected_card_id = "jo8qijTo"
         self.card_comparer_test(compared_card_id, expected_card_id, expected_result)
+
+    @parameterized.expand([
+        ("YldYBOxa",),
+        ("I9z6krUS", [CompareResultType.FAILED, CompareResultType.DOES_NOT_CONTAIN_ELEMENT,
+                      CompareResultType.HAS_EXTRA_ELEMENT])
+    ])
+    def test_card_has_attachments(self, compared_card_id, expected_result=None):
+        expected_card_id = "3lS576gC"
+        self.card_comparer_test(compared_card_id, expected_card_id, expected_result)
+
+    @parameterized.expand([
+        ("REjsydqK",),
+        ("AAAeeUAw", [CompareResultType.FAILED, CompareResultType.INVALID_VALUE])
+    ])
+    def test_card_was_archived(self, compared_card_id, expected_result=None):
+        expected_card_id = "BnBcwd5E"
+        self.card_comparer_test(compared_card_id, expected_card_id, expected_result)
+
+    @parameterized.expand([
+        ("ZCugeiMP",),
+        ("CFwMzfS1", [CompareResultType.FAILED, CompareResultType.INVALID_VALUE])
+    ])
+    def test_description_without_link(self, compared_card_id, expected_result=None):
+        expected_card_id = "RShk1cfR"
+        self.card_comparer_test(compared_card_id, expected_card_id, expected_result)
+
+    @parameterized.expand([
+        ("hNgThNym",),
+        ("ytmkj5qE", [CompareResultType.FAILED, CompareResultType.INVALID_VALUE])
+    ])
+    def test_comment_without_mentions(self, compared_card_id, expected_result=None):
+        expected_card_id = "jtv2DrFZ"
+        self.card_comparer_test(compared_card_id, expected_card_id, expected_result)
