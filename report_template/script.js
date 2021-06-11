aac5d8451465e857bf9c54eadfc32244d23437f1
Denis Bautin <denisbduis@gmail.com>
2021-06-12 02:33:10 +0500

    Fix animations

--- report_template/script.js
+++ report_template/script.js
@@ -10,5 +10,23 @@ function handleExpandButtonClick(event) {
 
     let sectionWithDetails = currentButton.parentNode.parentNode;
     let comparisonsList = sectionWithDetails.querySelector(".comparisons");
-    comparisonsList.classList.toggle("active");
+    animateComparisonsList(comparisonsList)
+}
+
+function animateComparisonsList(comparisons) {
+    if (comparisons.classList.contains("hidden")) {
+        comparisons.classList.remove("hidden");
+        setTimeout(function () {
+                comparisons.classList.add("active");
+            }, 2);
+    } else {
+        comparisons.classList.remove("active");
+        comparisons.addEventListener("transitionend", addHiddenClassOnce);
+    }
+}
+
+function addHiddenClassOnce(event) {
+    let target = event.currentTarget
+    target.classList.add("hidden")
+    target.removeEventListener("transitionend", addHiddenClassOnce)
 }
