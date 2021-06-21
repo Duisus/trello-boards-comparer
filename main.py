d1e860bb4ae094e394e77ed359c1b9439741a8da
Maksimymis <belyakevich@outlook.com>
2021-06-22 02:01:16 +0500

    Add Check Trello Lab function

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from trello import *
 
+from classes import method_cache_manager, render
+from classes.compare import default_comparers_provider
+from classes.mark_calculation import board_elements_counter, mark_calculator
 
 CONFIG_FILE = "config.json"
 
 
-if __name__ == "__main__":
+def checktrellolab(board_id_actual):
     with open(CONFIG_FILE, "r") as file:
         config_data = json.load(file)
 
@@ -13,7 +16,14 @@ if __name__ == "__main__":
         token=config_data["token"]
     )
 
-    board_id = "id of board"
-    board = client.get_board(board_id)
+    board_actual = client.get_board(board_id_actual)
+    board_id_expected = "E4ew5UjV"  # хардкод (temporary)
+    board_expected = client.get_board(board_id_expected)
+    mcm_expected = method_cache_manager.MethodCacheManager(board_expected)
+    comparer = default_comparers_provider.DefaultComparersProvider.create_board_comparer()
+    compare_result = comparer.start_compare(board_actual, mcm_expected)
+    num_of_checks = board_elements_counter.BoardElementsCounter.count(mcm_expected)
+    grade = mark_calculator.MarkCalculator.calculate(num_of_checks, compare_result)
+    render.get_report(compare_result, grade)
 
-    input()
+    return {grade, render.get_report(compare_result, grade)}
